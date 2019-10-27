1538e172cc48f4c82310cd248b08e1c3de550789
Marta Torre <hola@martatorre.dev>
2019-10-27 02:01:36 +0200

    Update custom-login.php

--- mu-plugins/custom-login.php
+++ mu-plugins/custom-login.php
@@ -2,25 +2,25 @@
 
 /**
  * Plugin name: Personalización del login de WordPress
- * Plugin URI: http://www.goodberries.com/
+ * Plugin URI: http://www.avanteconsultoria.com/
  * Description: Plugin de funcionalidades personalizadas para el login de WordPress
  * Version: 1.0
  * Author: OnDesarrollo
  * Author URI: https://ondesarrollo.com
- * License: GPL+2
+ * License: GPL+3
  * 
  */
 
 //Modificar el logo del login
-add_action('login_enqueue_scripts','gb_login_logo');
+add_action('login_enqueue_scripts','ac_login_logo');
 
-function gb_login_logo(){
+function ac_login_logo(){
 
 	?>
 
 	<style type="text/css" media="screen">
 		body.login div#login h1 a {
-			background-image: url('https://goodberries.ondesarrollo.dev/wp-content/uploads/2019/09/gb-web-logo.png');
+			background-image: url('#');
 			background-size: 300px;
 			height: 200px;
 			width: auto;
@@ -31,17 +31,17 @@ function gb_login_logo(){
 }
 
 //Modificar la url del logo
-add_filter ('login_headerurl','gb_login_logo_link');
+add_filter ('login_headerurl','ac_login_logo_link');
 
-function gb_login_logo_link($url){
+function ac_login_logo_link($url){
 
 	return home_url();
 }
 
 //Modificar el texto del logo
-add_filter('login_headertitle','gb_login_logo_title');
+add_filter('login_headertitle','ac_login_logo_title');
 
-function gb_login_logo_title($message){
+function ac_login_logo_title($message){
 
 	$message = get_bloginfo('name');
 	return $message;
